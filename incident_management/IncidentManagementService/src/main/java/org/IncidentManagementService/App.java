20b70ca7b57a688cd96a895dcf32ae05f7e057d3
ManjulaAram <manjula.aram@incture.com>
2018-01-09 16:52:59 +0530

    Incident Management Initial

---
+++ incident_management/IncidentManagementService/src/main/java/org/IncidentManagementService/App.java
@@ -0,0 +1,13 @@
+package org.IncidentManagementService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
